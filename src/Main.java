6fc7916ddae6287662ed08834ed45d69d2e082dc
Mikael Rönnberg <mikael.rnnberg@gmail.com>
2024-12-17 13:26:00 +0100

    Pushing my solution to the exercise

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,47 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        String firstName = "Mikael";
-        String lastName = "Rönnberg";
-        int age = 37;
-        double length = 180.00f;
-        double weight = 80;
-        boolean isStudent = true;
+        //Mata in temperatur i celsius
+        //Spara de 5 senaste temperaturerna
+        //Räkna ut medelvärdet av de 5
+        double[] savedTemps = new double[5];
+        Scanner tempInput = new Scanner(System.in);
+        for(int i = 0; i < savedTemps.length; i++) {
+            while(true) {
+                try{
+                    System.out.println("Input temperature " + (i + 1) + " in Celsius ");
+                    String input = tempInput.nextLine();
+                    double temp = Double.parseDouble(input);
+                    savedTemps[i] = convertToFahrenheit(temp);
+                    //System.out.println("Your temperature in Farenheit is " + savedTemps[i]) ;
+                    break;
+                } catch(NumberFormatException e) {
+                    System.out.println("Invalid input! Please enter a valid number.");
+                }
+            }
+        }
+        double median = 0;
+        double[] celsiusTemps = new double[5];
+        for(int i = 0; i < savedTemps.length; i++) {
+            median += savedTemps[i];
+            celsiusTemps[i] = convertToCelsius(savedTemps[i]);
+            System.out.println("Temperature in index " +(i+1)+ " is " + savedTemps[i] + " Fahrenheit");
+            //System.out.println(Arrays.toString(savedTemps));
+        }
+
+        System.out.println("Avarage temperature is " + median/savedTemps.length + " Fahrenheit, and is " + convertToCelsius(median/savedTemps.length) + " in Celsius");
+
+        for(int i = 0; i < celsiusTemps.length; i++) {
+            System.out.println("Temperature in index " +(i+1)+ " is " + celsiusTemps[i] + " Celsius");
+            //System.out.println(Arrays.toString(savedTemps));
+        }
     }
 
+    static double convertToFahrenheit(double temp1) {
+        return (temp1 * 1.8) + 32;
+    }
+    static double convertToCelsius (double temp2) {
+        return (temp2 - 32) / 1.8;
+    }
 }
-
-//1. Deklarera variabler för följande personuppgifter:
-//- Förnamn och efternamn (separata variabler) - Ålder
-//- Längd i meter
-//- Vikt i kg
-//- Om personen är student (ja/nej)
-//2. Tilldela värden till alla variabler:
-//- Välj lämplig datatyp för varje variabel
-//- Skriv en kommentar vid varje variabel som förklarar ditt val av datatyp - Tänk på att längd behöver decimaler medan ålder är heltal
-//3. Skriv ut informationen:
-//- Använd System.out.println för varje utskrift
-//- Testa både att skriva ut variabler direkt och att kombinera text med variabler - Experimentera med olika sätt att formatera utskrifterna
-//4. Gör några enkla beräkningar:
-//- Räkna ut personens ålder om 5 år
-//- Konvertera längden till centimeter
-//- Experimentera med att kombinera olika datatyper i beräkningarna
-//Saker att testa och reflektera över:
-//- Vad händer om du försöker spara ett decimaltal i en heltalsvariabel?
-//- Vad händer om du försöker använda ett variabelnamn som börjar med en siffra? - Prova att deklarera flera variabler på samma rad - hur fungerar det?
-//- Testa att ändra värden på variabler efter att du först tilldelat dem värden
-//Detta lär dig:
-//- Syntax för variabeldeklaration
-//- Namngivning av variabler
-//- Olika sätt att skriva ut information
-//- Grundläggande datatyper
-//- Hur man skriver kommentarer
-//- Hur man gör enkel formatering av utskrifter - Grundläggande operatorer och beräkningar
-//Tips:
-//- Ta dig tid att experimentera med koden
-//- Skriv kommentarer som förklarar vad du gör
-//- Testa att göra samma sak på olika sätt
-//- Försök medvetet skapa några fel för att se vad som händer - Var inte rädd för att testa dig fram
